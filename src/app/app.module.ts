0e0c747d0cd73edf7537be74e3cac08d1480b390
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2022-04-05 16:48:48 -0500

    applying lesson 5 part one till page 11 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "games",
+        component: GamesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor(private _router:Router) { 
+    
+  }
+
+  onHome() :void{
+    this._router.navigate([""])
+  }
+  onGames() :void{
+    this._router.navigate(["games"])
+  }
+  ngOnInit(): void {
+  }
+
+}
