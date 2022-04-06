2ee0407f01e63e4dc3458eb0cde8983491d2c145
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2022-04-06 13:03:43 -0500

    games data

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { GamesDataService } from '../games-data.service';
+
+export class Game{
+  private _id! : String
+  private title! : String
+  private year! : Number
+  private rate! : Number
+  private price! : Number
+  private minPlayers! : Number
+  private maxPlayers! : Number
+  private minAge! : Number
+  
+  // set title(title:String){this.title=title} 
+  constructor(_id: String, title : String,price : Number,){
+    this._id = _id 
+    this.title =  title
+    this.price = price
+  }
+}
 
 @Component({
   selector: 'app-games',
@@ -7,9 +26,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-  constructor() { }
+  // let games:Game=  new Game("1","Catan",35)
+  games!: Game[]
+
+  constructor(private gamesService:GamesDataService) {
+    
+    // this.games = new Array<Game>(2)
+    // this.games[0] = new Game("1","Catan",39.00)
+    // this.games[1] = new Game("2","King of you",39.00)
+    
+  }
 
   ngOnInit(): void {
+    this.games = this.gamesService.getGames()
   }
 
 }
